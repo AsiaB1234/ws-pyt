017866f5f5bb3c29f9e4d35242b609d4d419f87b
jbednarko <jbednarko@localhost>
2017-06-16 18:37:14 +0200

    Add categories to useView

--- src/main/java/pyt/view/UserView.java
+++ src/main/java/pyt/view/UserView.java
@@ -3,6 +3,7 @@ package pyt.view;
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
+import pyt.model.Category;
 import pyt.model.Project;
 import pyt.model.Task;
 import pyt.model.User;
@@ -13,15 +14,17 @@ public class UserView implements Serializable {
     private String name;
     private List<Task> tasks = new LinkedList<>();
     private List<Project> projects = new LinkedList<>();
+    private List<Category> categories = new LinkedList<>();
 
     public UserView() {
     }
 
-    public UserView(Long id, String name, List<Task> tasks, List<Project> projects) {
+    public UserView(Long id, String name, List<Task> tasks, List<Project> projects, List<Category> categories) {
         this.id = id;
         this.name = name;
         this.tasks = tasks;
         this.projects = projects;
+        this.categories = categories;
     }
 
     public UserView(User user) {
@@ -29,6 +32,7 @@ public class UserView implements Serializable {
         this.name = user.getName();
         this.tasks = user.getTasks();
         this.projects = user.getProjects();
+        this.categories = user.getCategories();
     }
 
     public Long getId() {
@@ -45,6 +49,10 @@ public class UserView implements Serializable {
 
     public List<Project> getProjects() {
         return projects;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
     }
 
 }
